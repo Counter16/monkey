8035feae3073ed80257b3a272e56a338fd98fb9f
Counter16 <awang16@ladueschools.net>
2023-01-09 04:47:55 +0000

    dddddddddddddddd

--- 3.7.4/Main.java
+++ 3.7.4/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 public class Main{
     public static void main(String args []){
-        
+        HorseBarn barn = new HorseBarn(); //make new horsebarn 
+        ArrayList<Horse> example1 = RandomPermutation.next(barn.getSpaces()); //make new horse arraylist
+        System.out.println(example1); //random list of horsies
+
+        if (example1.size() % 2 != 0){
+            example1.add(new Horse("nobody else unfortunately", 0));
+        } 
+        for (int i = 0; i < example1.size(); i += 2) {
+            System.out.println(example1.get(i) + " and " + example1.get(i + 1)); //prints pairs, i added one horsie to .txt so someone would always be alone
+
     }
+}
 }
--- 3.7.4/RandomPermutation.java
+++ 3.7.4/RandomPermutation.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class RandomPermutation
 {
   /**
@@ -7,23 +9,28 @@ public class RandomPermutation
    *
    * Initial conditions: length cannot be negative
    */
-   public static int[] next(int length)
-   {
-      int[] r = new int[length];
-      int[] p = new int[length];
+  public static ArrayList<Horse> next(ArrayList<Horse> horses) { 
+    int length = horses.size();
+   
       
-      for (int i = 0; i < length; i++)
-        p[i] = i + 1;
+    int[] r = new int[length];
+    int[] p = new int[length];
+      
+    for (int i = 0; i < length; i++)
+      p[i] = i + 1;
         
-      for (int n = length; n > 0; n--)
-      {
-        int pos = (int) (Math.random() * n);
-        r[n-1] = p[pos];
-        p[pos] = p[n-1];
-      }
-      
+    for (int n = length; n > 0; n--)
+    {
+      int pos = (int) (Math.random() * n);
+      r[n-1] = p[pos];
+      p[pos] = p[n-1];
+    }
+    ArrayList<Horse> example = new ArrayList<Horse>();
+    for (int i : r) {
+      example.add(horses.get(i - 1));
+    }
       // debug: System.out.println(java.util.Arrays.toString(r));
-      return r;
+    return example;
    }
 
 }
