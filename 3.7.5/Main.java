cb87e4ba97cd739d551491a78f72965e01737af7
Counter16 <awang16@ladueschools.net>
2023-01-11 14:00:23 +0000

    a0idoaidnoaindoin

--- 3.7.5/Main.java
+++ 3.7.5/Main.java
@@ -2,10 +2,19 @@ public class Main {
     public static void main(String args []){
         String authorToFind = "L. Frank Baum";
         Library mlonkkey = new Library();
-        for(Book mloookey : mlonkkey.getChildrensBooks()){
-            if(authorToFind.equals(mlonkkey.getAuthor())){
-                System.out.println(mlonkkey.getAuthor());
+        for(Book b : mlonkkey.getChildrensBooks()){
+            if(authorToFind.equals(b.getAuthor())){
+                System.out.println(b.getTitle());
             }
-    }
+        }
+        String bookToFind = "Sky Island";
+        double minimumRating = 0.0;
+        for(Book c : mlonkkey.getChildrensBooks()){
+            if(c.getTitle().equals(bookToFind)){
+                minimumRating = c.getRating();
+            }
+        }
+        System.out.println(minimumRating);
+    
     }
 }
